8d14b907c71fc9cc85b89280ccf6a2e588094915
Robert DeVore <deviodigital@gmail.com>
2020-11-26 23:50:05 -0500

    :package: NEW: 2 new filters added to the shortcode

--- admin/docupress-shortcodes.php
+++ admin/docupress-shortcodes.php
@@ -34,28 +34,34 @@ function docupress_shortcode( $atts ) {
     global $post;
     
     if ( 'all' === $collections ) {
-        $docupress_articles = new WP_Query(
-            array(
-                'post_type' => 'docupress',
-                'showposts' => $limit,
-                'orderby'   => $order,
-            )
+        // Args.
+        $args = array(
+            'post_type' => 'docupress',
+            'showposts' => $limit,
+            'orderby'   => $order,
         );
+        // Filter args.
+        $args = apply_filters( 'docupress_shortcode_query_args', $args );
+        // Get results.
+        $docupress_articles = new WP_Query( $args );
     } else {
-        $docupress_articles = new WP_Query(
-            array(
-                'post_type' => 'docupress',
-                'showposts' => $limit,
-                'orderby'   => $order,
-                'tax_query' => array(
-                    array(
-                        'taxonomy' => 'docupress_collections',
-                        'field'    => 'slug',
-                        'terms'    => $collections
-                    ),
+        // Args.
+        $args = array(
+            'post_type' => 'docupress',
+            'showposts' => $limit,
+            'orderby'   => $order,
+            'tax_query' => array(
+                array(
+                    'taxonomy' => 'docupress_collections',
+                    'field'    => 'slug',
+                    'terms'    => $collections
                 ),
-            )
+            ),
         );
+        // Filter args.
+        $args = apply_filters( 'docupress_shortcode_query_args_collections', $args );
+        // Get results.
+        $docupress_articles = new WP_Query( $args );
     }
 
     $docupress_list = '<ul class="docupress-shortcode-list">';
@@ -63,19 +69,17 @@ function docupress_shortcode( $atts ) {
     // Loop through the articles.
     while ( $docupress_articles->have_posts() ) : $docupress_articles->the_post();
         $docupress_list .= '<li>';
-        $docupress_list .= "<a href='" . esc_url( get_permalink( $post->ID ) ) . "' class='docupress-shortcode-link'>" . get_the_title( $post->ID ) . "</a>";
+        $docupress_list .= '<a href="' . esc_url( get_permalink( $post->ID ) ) . '" class="docupress-shortcode-link">' . get_the_title( $post->ID ) . '</a>';
         $docupress_list .= '</li>';
     endwhile; // End loop.
 
     wp_reset_postdata();
 
     // Website link.
-    $websitelink = get_bloginfo( 'url' );
-
     if ( 'all' !== $collections ) {
         if ( 'on' === $viewall ) {
             $docupress_list .= '<li>';
-            $docupress_list .= "<a href='" . $websitelink . "/collections/" . $collections . "'>" . __( 'view all', 'docupress' ) . " &rarr;</a>";
+            $docupress_list .= '<a href="' . get_bloginfo( 'url' ) . '/collections/' . $collections . '">' . __( 'view all', 'docupress' ) . ' &rarr;</a>';
             $docupress_list .= '</li>';
         }
     }
